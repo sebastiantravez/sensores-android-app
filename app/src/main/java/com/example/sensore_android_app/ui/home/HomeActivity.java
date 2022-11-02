0b6d4a1ffa86b4a57ef78961cc13290c10903214
SebastianTravez1995 <carlos.travez@rappi.com>
2022-11-01 22:42:55 -0500

    login con firebase

---
+++ app/src/main/java/com/example/sensore_android_app/ui/home/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.sensore_android_app.ui.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.sensore_android_app.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
