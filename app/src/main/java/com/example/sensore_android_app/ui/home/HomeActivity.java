fe951b818043398d199b70ce576617288f30701c
SebastianTravez1995 <carlos.travez@rappi.com>
2022-11-04 14:06:05 -0500

    botnes en home

--- app/src/main/java/com/example/sensore_android_app/ui/home/HomeActivity.java
+++ app/src/main/java/com/example/sensore_android_app/ui/home/HomeActivity.java
@@ -1,16 +1,27 @@
 package com.example.sensore_android_app.ui.home;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageView;
 
 import com.example.sensore_android_app.R;
 
+
 public class HomeActivity extends AppCompatActivity {
 
+    AppCompatImageView imageButtonHum;
+    AppCompatImageView imageButtonTem;
+    AppCompatImageView imageButtonLum;
 
+    @SuppressLint({"WrongViewCast", "MissingInflatedId"})
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +29,31 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        imageButtonHum = findViewById(R.id.btnHumedad);
+        imageButtonTem = findViewById(R.id.btnTemperatura);
+        imageButtonLum = findViewById(R.id.btnLuminosidad);
 
+        imageButtonHum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeActivity.this, HumedadActivity.class));
+            }
+        });
+
+        imageButtonTem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeActivity.this, TemperaturaActivity.class));
+            }
+        });
+
+        imageButtonLum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeActivity.this, LuminosidadActivity.class));
+            }
+        });
     }
-
 
 
 }
