5a9de6e6b24ef2d8489e1a6e894dddc9fee5e603
SebastianTravez1995 <carlos.travez@rappi.com>
2022-11-08 15:01:11 -0500

    splash screeen

--- app/src/main/java/com/example/sensore_android_app/ui/main/MainActivity.java
+++ app/src/main/java/com/example/sensore_android_app/ui/main/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        setTheme(R.style.Theme_Sensoreandroidapp);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         startActivity(new Intent(this, LoginActivity.class));
